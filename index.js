408376dd7f75bd9845f76671d283b0375d80d720
ibrahim <ihussein292@gmail.com>
2023-04-05 15:42:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+    const data = { name, email };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, configObj)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.append(id);
+      })
+      .catch(error => {
+        document.body.append(error.message);
+      });
+  }
+  
